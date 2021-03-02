7e5ba1a531b76ee1523265ecfa596bf5b04c3321
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-03-02 23:47:28 +0900

    CarController & Car Test
    - CarController test occurred an error for test event not found

--- src/main/java/com/junit/test/controller/CarController.java
+++ src/main/java/com/junit/test/controller/CarController.java
@@ -1,4 +1,15 @@
 package com.junit.test.controller;
 
+import com.junit.test.domain.Car;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
+
+    @GetMapping("/cars/{name}")
+    private Car getCar(@PathVariable String name) {
+        return null;
+    }
 }
--- src/test/java/com/junit/test/CarControllerTest.java
+++ src/test/java/com/junit/test/CarControllerTest.java
@@ -9,6 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(CarController.class)
 public class CarControllerTest {
@@ -19,6 +22,9 @@ public class CarControllerTest {
     @Test
     public void getCar_ShouldReturnCar() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/cars/prius"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("name").value("prius"))
+                .andExpect(jsonPath("type").value("hybrid"));
 
     }
 }
